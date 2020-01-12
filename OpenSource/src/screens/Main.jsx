418256153531df4d6e416dfad9fb2d5f128f66af
gmsy <gilm898@gmail.com>
2020-01-11 22:17:04 -0500

    Install deck-swiper

--- OpenSource/src/screens/Main.jsx
+++ OpenSource/src/screens/Main.jsx
@@ -1,13 +1,33 @@
 import React, { Fragment, Component } from "react";
 import { View, Text } from "react-native";
+import Swiper from "react-native-deck-swiper";
 import ui from "../style_sheets";
+import { Card1, Card2, Card3, Card4 } from "./Cards";
 
 export default class Main extends Component {
-  render = () => (
-    <Fragment>
+  render() {
+    return (
       <View style={ui.container}>
-        <Text>Main.jsx</Text>
+        <Swiper
+          cards={[<Card1 />, <Card2 />, <Card3 />, <Card4 />]}
+          renderCard={card => {
+            return (
+              <View style={ui.card}>
+                <Text style={ui.text}>{card}</Text>
+              </View>
+            );
+          }}
+          onSwiped={cardIndex => {
+            console.log(cardIndex);
+          }}
+          onSwipedAll={() => {
+            console.log("onSwipedAll");
+          }}
+          cardIndex={0}
+          backgroundColor={"#4FD0E9"}
+          stackSize={3}
+        ></Swiper>
       </View>
-    </Fragment>
-  );
+    );
+  }
 }
--- OpenSource/src/style_sheets/index.js
+++ OpenSource/src/style_sheets/index.js
@@ -1,10 +1,32 @@
 import { StyleSheet } from "react-native";
 
-export default styles = StyleSheet.create({
+export default ui = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#F5FCFF",
+    alignContent: "center",
+    justifyContent: "center",
+    alignContent: "center"
+  },
+  card: {
+    flex: 1,
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: "silver",
+    justifyContent: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.9)",
+    alignSelf: "center",
+    width: "90%",
+    height: "40%"
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 70,
+    backgroundColor: "transparent"
+  },
+  textBox: {
+    textAlign: "center",
+    fontSize: 30,
+    backgroundColor: "transparent"
   }
 });
